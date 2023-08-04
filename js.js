2b5c58f701ce6134bac61e72159ecfbfa23bf7f9
Longngok <bboylongngok193@gmail.com>
2023-08-04 10:17:21 +0700

    first commit

--- js.js
+++ js.js
@@ -1,4 +1,7 @@
-function submit()
-{
-    alert('Thanks for submit');
+function togglebar() {
+    var navResponsiveElements = document.getElementsByClassName('dropdown-menu');
+  
+    for (var i = 0; i < navResponsiveElements.length; i++) {
+      navResponsiveElements[i].classList.toggle('active');
+    }
 }
